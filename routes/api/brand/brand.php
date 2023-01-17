ff311f625caba71db5b3783a3edc5460a44c49fa
gezrylclariz <gezrylclarizg@gmail.com>
2023-01-17 21:25:04 +0800

    Update brand.php

--- routes/api/brand/brand.php
+++ routes/api/brand/brand.php
@@ -5,9 +5,10 @@ use App\Http\Controllers\Brand\BrandController;
 
 Route::prefix('brands')->group(function() {
     Route::get('/', [BrandController::class, 'index'])->name('brand.list');
+    Route::get('/archives', [BrandController::class, 'archives'])->name('brand.archives');
     Route::get('/{brand}', [BrandController::class, 'show'])->name('brand.show');
     Route::post('/', [BrandController::class, 'store'])->name('brand.store');
     Route::put('/{brand}', [BrandController::class, 'update'])->name('brand.update');
-    Route::delete('/{brand}', [BrandController::class, 'archive'])->name('brand.archive');
+    Route::delete('/{brand}', [BrandController::class, 'delete'])->name('brand.delete');
     Route::get('/restore/{brand}', [BrandController::class, 'restore'])->name('brand.restore');
 });
