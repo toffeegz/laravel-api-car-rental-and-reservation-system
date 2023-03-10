514abc3e850ad3be451994700c03a79888933814
gezrylclariz <gezrylclarizg@gmail.com>
2023-03-10 21:38:00 +0800

    added column 'type' in verifications table;

--- database/migrations/2023_03_08_221149_create_verifications_table.php
+++ database/migrations/2023_03_08_221149_create_verifications_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('token');
             $table->bigInteger('user_id');
+            $table->string('type');
             $table->timestamps();
         });
     }
