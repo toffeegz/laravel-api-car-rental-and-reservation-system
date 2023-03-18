c44758b936224b206bbdf4d7edf3a3f0514f8186
gezrylclariz <gezrylclarizg@gmail.com>
2023-03-18 09:14:35 +0800

    fix Traits CreatedBy;

--- app/Traits/CreatedBy.php
+++ app/Traits/CreatedBy.php
@@ -3,11 +3,12 @@
 namespace App\Traits;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 trait CreatedBy {
     protected static function bootCreatedBy() {
-        static::saving(function ($model) {
-            if(isset($model['created_by']) && Auth::check()) {
+        static::creating(function ($model) {
+            if(is_null($model->created_by) && Auth::check()) {
                 $model->created_by = Auth::user()->id;
             }
         });
